4adf4de05d1124e8796b131984003835da22d584
spookTacularLDN43 <117355850+spookTacularLDN43@users.noreply.github.com>
2023-02-09 14:28:05 +0100

    CommonsCodec

--- src/it/login/database/UsersRepository.java
+++ src/it/login/database/UsersRepository.java
@@ -1,5 +1,7 @@
 package it.login.database;
 import it.login.model.User;
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +9,7 @@ public class UsersRepository {
     public List<User> userList = new ArrayList<>();
     private static final UsersRepository usersRepository = new UsersRepository();
     private UsersRepository() {
-        this.userList.add(new User("admin", "admin"));
+        this.userList.add(new User("admin", DigestUtils.md5Hex("admin")));
     }
 
     public static UsersRepository getInstance() {
@@ -16,7 +18,7 @@ public class UsersRepository {
     public boolean authenticate(String login, String password){
         for (User currentUser : userList) {
             if(currentUser.getLogin().equals(login)){
-                if(currentUser.getPass().equals(password)){
+                if(currentUser.getPass().equals(DigestUtils.md5Hex(password))){
                     return true;
                 }else {
                     return false;
@@ -31,7 +33,7 @@ public class UsersRepository {
                 return false;
             }
         }
-        this.userList.add(new User(login, pass));
+        this.userList.add(new User(login, DigestUtils.md5Hex(pass)));
         return true;
     }
 }
